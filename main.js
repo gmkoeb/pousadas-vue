99b6e98751218673628d77fcfc2c493af3d074a7
Gabriel Manika Koeb <gmkoeb@gmail.com>
2023-11-24 18:10:27 -0300

    Inclui renderização dinâmica de quartos para cada pousada

--- main.js
+++ main.js
@@ -17,19 +17,19 @@ const app = Vue.createApp({
       let inn = await response.json()
       inn.payment_methods = JSON.parse(inn.payment_methods)
       this.innDetails = inn
+      this.innRooms = await this.getRooms(inn_id)
      },
      hideInnDetails(){
       this.innDetails = {}
      },
-     async getRooms(){
-      let response = await fetch('http://localhost:3000/api/v1/inns/1/rooms')
+     async getRooms(inn_id){
+      let response = await fetch(`http://localhost:3000/api/v1/inns/${inn_id}/rooms`)
       let rooms = await response.json()
       return rooms
      }
     },
     async mounted() {
-      this.innsList = await this.getInns(),
-      this.innRooms = await this.getRooms()
+      this.innsList = await this.getInns()
     },
 })
 
